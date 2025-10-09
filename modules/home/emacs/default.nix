2577e3bbb118af609bc66653c2c9b290e201bc74
aahsnr-configs <ahsanur041@proton.me>
2025-10-08 22:07:56 -0400

    updated

--- modules/home/emacs/default.nix
+++ modules/home/emacs/default.nix
@@ -3,6 +3,27 @@
   pkgs,
   ...
 }:
+
+let
+  # The emacs-overlay provides a function to parse your emacs config
+  # and install the declared packages.
+  emacs = pkgs.emacsWithPackagesFromUsePackage {
+    config = ./init.el;
+    # You can choose your preferred emacs version.
+    package = pkgs.emacs30-pgtk;
+    # `alwaysEnsure` is equivalent to setting `use-package-always-ensure` to `t`.
+    alwaysEnsure = true;
+    # `alwaysTangle` will tangle all code blocks that do not have a `:tangle`
+    # argument. This is useful when you have a `#+PROPERTY: header-args:emacs-lisp :tangle yes`
+    # in your org file.
+    alwaysTangle = false;
+    # You can also add extra packages that are not in your config file.
+    extraEmacsPackages = epkgs: [
+      epkgs.vterm
+    ];
+  };
+
+in
 {
   imports = [ inputs.nix-doom-emacs-unstraightened.hmModule ];
 
