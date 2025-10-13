bf428bad136a5f773e467b98894bb4e469234ffc
aahsnr-configs <ahsanur041@proton.me>
2025-10-13 07:12:13 -0400

    debugging Hyprland

--- modules/home/hyprland/monitor.nix
+++ modules/home/hyprland/monitor.nix
@@ -1,12 +1,9 @@
 { ... }:
 {
   wayland.windowManager.hyprland.settings = {
-    monitorv2 = {
-      output = "HDMI-A-1";
-      mode = "1920x1080@60";
-      position = "0x0";
-      scale = 1.00;
-    };
+    monitor = [
+      "HDMI-A-1,1920x1080@60,0x0,1"
+    ];
     experimental = {
       xx_color_management_v4 = true;
     };
